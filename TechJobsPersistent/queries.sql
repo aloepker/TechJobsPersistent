16eabc215b1e08dd42693dd68f2ae9ae73a48d10
aloepker <91484770+aloepker@users.noreply.github.com>
2022-01-14 15:06:17 -0600

    updated sql querys, still need help with part 3

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,11 +1,17 @@
 --Part 1
-INT ID, VARCHAR Name, INT EmployerId
+--list  columns and data types in the Jobs table
+ID is an integer, Name is a VARCHAR, EmployerId is also an Int
 
 --Part 2
-
 --SQL TASK: write a query to list the names of the employers in St. Louis City
-
+SELECT Name
+FROM Employers
+WHERE Location="St. Louis";
 --Part 3
 
 --SQL TASK:write a query to return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order. 
 --If a skill does not have a job listed, it should not be included in the results of this query. make use of "is not null"
+SELECT Name, Description
+FROM skills
+WHERE job IS NOT NULL
+ORDER BY Name;
